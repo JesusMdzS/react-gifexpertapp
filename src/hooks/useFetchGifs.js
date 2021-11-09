57530431012e6dbce1da7d9ac62b11c5b2249e2e
Jesus Mendoza <jmendoza@quaxar.com>
2021-11-08 21:12:09 -0600

    actualizacion

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 export const useFetchGifs = (category) => {
   const [state, setstate] = useState({
