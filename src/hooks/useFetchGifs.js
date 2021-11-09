0be89c66e44871833ee36a95fa174d1367a0deb9
Jesus Mendoza <jmendoza@quaxar.com>
2021-11-08 20:18:18 -0600

    fetch rellamado

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+
 export const useFetchGifs = (category) => {
   const [state, setstate] = useState({
     data: [],
