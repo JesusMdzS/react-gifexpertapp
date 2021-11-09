b4f44926d20723c1ee46e56b669ecec61d98ab6f
Jesus Mendoza <jmendoza@quaxar.com>
2021-11-08 20:02:38 -0600

    v1 fetch

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,33 +1,18 @@
-import { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+export const useFetchGifs = (category) => {
+  const [state, setstate] = useState({
+    data: [],
+    loading: true,
+  });
 
-
-export const useFetchGifs = ( category ) => {
-    
-    const [state, setState] = useState({
-        data: [],
-        loading: true
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setstate({
+        data: imgs,
+        loading: false,
+      });
     });
-
-    useEffect( () => {
-
-        getGifs( category )
-            .then( imgs => {
-                
-                setState({
-                    data: imgs,
-                    loading: false
-                });
-            })
-
-    }, [category])
-
-
-
-
-    return state; // { data:[], loading: true };
-
-
-}
-
-
+  }, [category]);
+  return state; //{data:[], loading: true}
+};
