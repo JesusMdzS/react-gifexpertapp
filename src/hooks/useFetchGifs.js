ff20ceb48718789fba63cb86822ea6a378aa521f
Jesus Mendoza <jmendoza@quaxar.com>
2021-11-09 00:05:58 -0600

    actualizacion de ./

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import getGifs from "../helpers/getGifs";
+import { getGifs } from "../helpers/getGifs";
 export const useFetchGifs = (category) => {
-  const [state, setstate] = useState({
+  const [state, setState] = useState({
     data: [],
     loading: true,
   });
 
   useEffect(() => {
     getGifs(category).then((imgs) => {
-      setstate({
+      setState({
         data: imgs,
         loading: false,
       });
